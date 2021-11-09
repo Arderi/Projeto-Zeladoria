2899b0e748f1f3f698f5dfc670cf75315933c461
Arderi <93890449+Arderi@users.noreply.github.com>
2021-11-09 20:27:26 -0300

    Initial commit

---
+++ javascript.js
@@ -0,0 +1,311 @@
+function scroll_top() {
+    window.scroll({
+        top: 0, 
+        behavior: 'smooth' 
+      });
+}
+
+function scroll_5s() {
+    window.scroll({
+        top:700,
+        behavior: 'smooth' 
+      });
+}
+
+function scroll_thiago() {
+    window.scroll({
+        top: 1350, 
+        left: 0, 
+        behavior: 'smooth' 
+      });
+}
+
+function scroll_escola() {
+    window.scroll({
+        top: 2150, 
+        left: 0, 
+        behavior: 'smooth' 
+      });
+}
+
+
+
+
+if (screen.width < 1030) {
+
+
+  function scroll_5s() {
+      window.scroll({
+          top:600,
+          behavior: 'smooth' 
+        });
+  }
+  
+  function scroll_thiago() {
+      window.scroll({
+          top: 1100, 
+          left: 0, 
+          behavior: 'smooth' 
+        });
+  }
+  
+  function scroll_escola() {
+      window.scroll({
+          top: 2200, 
+          left: 0, 
+          behavior: 'smooth' 
+        });
+  }
+  
+  
+}
+
+
+
+
+
+
+
+
+if (screen.width < 900) {
+
+
+function scroll_5s() {
+    window.scroll({
+        top:1160,
+        behavior: 'smooth' 
+      });
+}
+
+function scroll_thiago() {
+    window.scroll({
+        top: 1800, 
+        left: 0, 
+        behavior: 'smooth' 
+      });
+}
+
+function scroll_escola() {
+    window.scroll({
+        top: 2970, 
+        left: 0, 
+        behavior: 'smooth' 
+      });
+}
+
+}
+
+
+if (screen.width < 780) {
+
+
+  function scroll_5s() {
+      window.scroll({
+          top:1000,
+          behavior: 'smooth' 
+        });
+  }
+  
+  function scroll_thiago() {
+      window.scroll({
+          top: 1680, 
+          left: 0, 
+          behavior: 'smooth' 
+        });
+  }
+  
+  function scroll_escola() {
+      window.scroll({
+          top: 2600, 
+          left: 0, 
+          behavior: 'smooth' 
+        });
+  }
+  
+  
+}
+
+
+
+
+
+if (screen.width < 550) {
+
+
+  function scroll_5s() {
+      window.scroll({
+          top:1200,
+          behavior: 'smooth' 
+        });
+  }
+  
+  function scroll_thiago() {
+      window.scroll({
+          top: 1860, 
+          left: 0, 
+          behavior: 'smooth' 
+        });
+  }
+  
+  function scroll_escola() {
+      window.scroll({
+          top: 2930, 
+          left: 0, 
+          behavior: 'smooth' 
+        });
+  }
+  
+  
+}
+
+
+
+
+
+
+if (screen.width < 420) {
+
+
+  function scroll_5s() {
+      window.scroll({
+          top:1000,
+          behavior: 'smooth' 
+        });
+  }
+  
+  function scroll_thiago() {
+      window.scroll({
+          top: 1800, 
+          left: 0, 
+          behavior: 'smooth' 
+        });
+  }
+  
+  function scroll_escola() {
+      window.scroll({
+          top: 2970, 
+          left: 0, 
+          behavior: 'smooth' 
+        });
+  }
+  
+  
+}
+
+if (screen.width < 415) {
+
+
+  function scroll_5s() {
+      window.scroll({
+          top:1400,
+          behavior: 'smooth' 
+        });
+  }
+  
+  function scroll_thiago() {
+      window.scroll({
+          top: 2100, 
+          left: 0, 
+          behavior: 'smooth' 
+        });
+  }
+  
+  function scroll_escola() {
+      window.scroll({
+          top: 3200, 
+          left: 0, 
+          behavior: 'smooth' 
+        });
+  }
+  
+  
+}
+
+
+if (screen.width < 380) {
+
+
+  function scroll_5s() {
+      window.scroll({
+          top:1500,
+          behavior: 'smooth' 
+        });
+  }
+  
+  function scroll_thiago() {
+      window.scroll({
+          top: 2200, 
+          left: 0, 
+          behavior: 'smooth' 
+        });
+  }
+  
+  function scroll_escola() {
+      window.scroll({
+          top: 3360, 
+          left: 0, 
+          behavior: 'smooth' 
+        });
+  }
+  
+  
+}
+
+if (screen.width < 365) {
+
+
+  function scroll_5s() {
+      window.scroll({
+          top:1600,
+          behavior: 'smooth' 
+        });
+  }
+  
+  function scroll_thiago() {
+      window.scroll({
+          top: 2300, 
+          left: 0, 
+          behavior: 'smooth' 
+        });
+  }
+  
+  function scroll_escola() {
+      window.scroll({
+          top: 3550, 
+          left: 0, 
+          behavior: 'smooth' 
+        });
+  }
+  
+  
+}
+
+if (screen.width < 325) {
+
+
+  function scroll_5s() {
+      window.scroll({
+          top:1750,
+          behavior: 'smooth' 
+        });
+  }
+  
+  function scroll_thiago() {
+      window.scroll({
+          top: 2400, 
+          left: 0, 
+          behavior: 'smooth' 
+        });
+  }
+  
+  function scroll_escola() {
+      window.scroll({
+          top: 3700, 
+          left: 0, 
+          behavior: 'smooth' 
+        });
+  }
+  
+  
+}
+
